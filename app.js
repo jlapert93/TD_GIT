fe5781548608c6148d1be697a79bd8eb16dfc046
elmabchourali <alielmabchour@gmail.com>
2024-01-26 12:43:18 +0100

    j'ai gardé le changement puis ajouter un plus

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 console.log('Bienvenue sur la branche ali el mabchour, Merci Ali ça fait plaisir :), plaisir partagé ^^');
 // j'ai gardé votre changement et ajouter un plus
-=======
-console.log('Voici sur la branche ali el mabchour, Merci Ali ça fait plaisir :)');
->>>>>>> 90276c1e9ce24c5700de4468bb5bc65dd63fa2f4
